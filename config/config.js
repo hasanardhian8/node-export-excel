364bc328fca8c196ce08cbed7d7466df4a9d78d0
hasanardhian8 <hasanardhian11@gmail.com>
2024-01-16 00:44:38 +0700

    migrate barang

---
+++ config/config.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.db_username,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    host: process.env.db_host,
+    dialect: process.env.db_dialect,
+  },
+  test: {
+    username: process.env.db_username,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    host: process.env.db_host,
+    dialect: process.env.db_dialect,
+  },
+  production: {
+    username: process.env.db_username,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    host: process.env.db_host,
+    dialect: process.env.db_dialect,
+  },
+};
