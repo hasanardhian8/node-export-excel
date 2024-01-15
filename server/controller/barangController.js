7d249761fa81d83d77532a4bd80e26be372de614
hasanardhian8 <hasanardhian11@gmail.com>
2024-01-16 01:29:45 +0700

    add & getAll

--- server/controller/barangController.js
+++ server/controller/barangController.js
@@ -1,12 +1,23 @@
-const db = require("../models");
-const Barang = db.barangs;
+const Barang = require("../models").Barang;
 
 exports.find = async (req, res) => {
   try {
-    console.log("kesini");
     const bar = await Barang.findAll();
-    console.log(bar);
     return res.send(bar);
+  } catch (error) {
+    return res.status(404).send("not found mauk");
+  }
+};
+
+exports.add = async (req, res) => {
+  const { nama, harga, stok } = req.body;
+  try {
+    const barang = await Barang.create({
+      nama,
+      harga,
+      stok,
+    });
+    res.status(201).json(barang);
   } catch (error) {
     return res.status(404).send("not found mauk");
   }
--- server/routers/barangRouter.js
+++ server/routers/barangRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { find } = require("../controller/barangController");
+const { find, add } = require("../controller/barangController");
 
 router.get("/barang", find);
+router.post("/barang", add);
 
 module.exports = router;
